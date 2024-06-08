00fd5e33e461bb82a5580705747246d8048a17ef
alejandro spirupulos <alejandrospirupulos99@gmail.com>
2024-06-08 20:09:16 -0300

    Agregar archivos de proyecto.

---
+++ estacionamiento/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace estacionamiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbGeneros = new System.Windows.Forms.ComboBox();
+            this.cmbPeliculas = new System.Windows.Forms.ComboBox();
+            this.lstPeliculas = new System.Windows.Forms.ListBox();
+            this.cmbGeneros2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnNuevaPelicula = new System.Windows.Forms.Button();
+            this.txtPelicula = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbGeneros
+            // 
+            this.cmbGeneros.FormattingEnabled = true;
+            this.cmbGeneros.Location = new System.Drawing.Point(201, 104);
+            this.cmbGeneros.Name = "cmbGeneros";
+            this.cmbGeneros.Size = new System.Drawing.Size(121, 21);
+            this.cmbGeneros.TabIndex = 0;
+            this.cmbGeneros.SelectedIndexChanged += new System.EventHandler(this.cmbgeneros_SelectedIndexChanged);
+            // 
+            // cmbPeliculas
+            // 
+            this.cmbPeliculas.FormattingEnabled = true;
+            this.cmbPeliculas.Location = new System.Drawing.Point(363, 104);
+            this.cmbPeliculas.Name = "cmbPeliculas";
+            this.cmbPeliculas.Size = new System.Drawing.Size(121, 21);
+            this.cmbPeliculas.TabIndex = 1;
+            // 
+            // lstPeliculas
+            // 
+            this.lstPeliculas.FormattingEnabled = true;
+            this.lstPeliculas.Location = new System.Drawing.Point(363, 178);
+            this.lstPeliculas.Name = "lstPeliculas";
+            this.lstPeliculas.Size = new System.Drawing.Size(120, 95);
+            this.lstPeliculas.TabIndex = 2;
+            // 
+            // cmbGeneros2
+            // 
+            this.cmbGeneros2.FormattingEnabled = true;
+            this.cmbGeneros2.Location = new System.Drawing.Point(201, 179);
+            this.cmbGeneros2.Name = "cmbGeneros2";
+            this.cmbGeneros2.Size = new System.Drawing.Size(121, 21);
+            this.cmbGeneros2.TabIndex = 3;
+            this.cmbGeneros2.SelectedIndexChanged += new System.EventHandler(this.cmbGeneros2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "GENEROS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "PELICULAS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "GENEROS";
+            // 
+            // btnNuevaPelicula
+            // 
+            this.btnNuevaPelicula.Location = new System.Drawing.Point(201, 245);
+            this.btnNuevaPelicula.Name = "btnNuevaPelicula";
+            this.btnNuevaPelicula.Size = new System.Drawing.Size(121, 23);
+            this.btnNuevaPelicula.TabIndex = 8;
+            this.btnNuevaPelicula.Text = "NUEVA PELICULA";
+            this.btnNuevaPelicula.UseVisualStyleBackColor = true;
+            this.btnNuevaPelicula.Click += new System.EventHandler(this.btnNuevaPelicula_Click);
+            // 
+            // txtPelicula
+            // 
+            this.txtPelicula.Location = new System.Drawing.Point(201, 206);
+            this.txtPelicula.Name = "txtPelicula";
+            this.txtPelicula.Size = new System.Drawing.Size(121, 20);
+            this.txtPelicula.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPelicula);
+            this.Controls.Add(this.btnNuevaPelicula);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbGeneros2);
+            this.Controls.Add(this.lstPeliculas);
+            this.Controls.Add(this.cmbPeliculas);
+            this.Controls.Add(this.cmbGeneros);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbGeneros;
+        private System.Windows.Forms.ComboBox cmbPeliculas;
+        private System.Windows.Forms.ListBox lstPeliculas;
+        private System.Windows.Forms.ComboBox cmbGeneros2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnNuevaPelicula;
+        private System.Windows.Forms.TextBox txtPelicula;
+    }
+}
+
---
+++ estacionamiento/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace estacionamiento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Creacion de arreglos de datos/ 
+
+        string[] generos = new string[3] { "Terror", "Accion", "Comedia" }; // formacion de la fila (vector)
+
+        string[,] peliculas = new string[3,3] {
+          {"T1","T2","T3"},
+          {"A1","A2","A3"},
+          {"C1","C2","C3"},
+
+        }; //fila 1 con generos 1 y matriz peliculas 1
+
+        string[,] peliculas2 = new string[3,3]; // matriz 2
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < generos.Length; i++)
+            {
+                cmbGeneros.Items.Add(generos[i]); // vector 1 
+                cmbGeneros2.Items.Add(generos[i]); // vector 2
+            }
+
+        }
+
+        private void cmbgeneros_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbPeliculas.Items.Clear();
+            int fila = cmbGeneros.SelectedIndex;
+            int columnas = 0;
+            while (columnas < peliculas.GetLength(1))
+            {
+                cmbPeliculas.Items.Add(peliculas[fila,columnas]);
+                columnas++;
+            } // creacion de bugle para matriz 1 con vector 1
+            
+        }
+
+        private void btnNuevaPelicula_Click(object sender, EventArgs e)
+        {
+            int filas2 =cmbGeneros2.SelectedIndex;
+            string nuevaPeliculas2 = txtPelicula.Text;
+            int columna = 0;
+            bool control = true;
+
+            while(columna < peliculas2.GetLength(1) && control == true)
+            {
+                if (peliculas2[filas2, columna] == null)
+                {
+                    peliculas2[filas2, columna] = nuevaPeliculas2;
+                    control = false;
+
+                } 
+                columna++;
+            }
+
+            
+
+        }
+
+        private void cmbGeneros2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstPeliculas.Items.Clear();    
+            int fila = cmbGeneros2.SelectedIndex;
+            int columnas = 0;
+            bool control = true;
+            while (columnas < peliculas2.GetLength(1) && control == true) {
+
+                if (peliculas2[fila, columnas] != null)
+                {
+                    lstPeliculas.Items.Add(peliculas2[fila, columnas]);
+                }
+                else
+                {
+                    control = false;
+                }
+                columnas++;
+            }
+        }
+    }
+}
